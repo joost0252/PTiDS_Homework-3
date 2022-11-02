a9bc8a95e990ad2c43581697b5ee6bd386d0f5f3
joost0252 <33458844+joost0252@users.noreply.github.com>
2022-11-02 17:48:30 +0100

    Function seems to work now. Added question b aswell.

--- Monte Carlo integration.R
+++ Monte Carlo integration.R
@@ -2,7 +2,7 @@ f <- function(x) {
   return(exp(x) * log(x))
 }
 
-find_integral <- function(f, xlim, N = 1000, seed = 10, make_plot = TRUE){
+find_integral <- function(f, xlim, N = 1000, seed = 10, make_plot = FALSE){
   if (missingArg(f)) {
     f <- function(x) {exp(x) * log(x)}
   }
@@ -16,7 +16,8 @@ find_integral <- function(f, xlim, N = 1000, seed = 10, make_plot = TRUE){
   
   # Simulate N points
   U = runif(N, min = 0.1, max = 2)
-  N=N
+  
+  fi <- list()
   for (i in 1:length(U)){
     fi[i] = f(U[i])
   }
@@ -37,3 +38,15 @@ find_integral <- function(f, xlim, N = 1000, seed = 10, make_plot = TRUE){
 }
 
 find_integral()
+
+
+# question b.
+# N = 10       gives: -0.6344028
+# N = 100      gives:  0.2654028
+# N = 1000     gives:  1.184418
+# N = 10000    gives:  1.105195
+# N = 100000   gives:  1.065235
+# N = 1000000  gives:  1.083766
+# N = 10000000 gives:  1.089825
+# As can be seen from 10^5, it does not have to be that it will get a better approximation. However, one can conclude that
+# in genereal the higher the N, the better approximation.
